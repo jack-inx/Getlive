0b4b0b627bb100df30744a5d8fe1c4f7112a1fb6
kchakravarthy <ror-inx@ubuntu4.(none)>
2013-04-12 16:10:27 +0530

    changes

--- db/migrate/20130401122844_rename_country_and_state.rb
+++ db/migrate/20130401122844_rename_country_and_state.rb
@@ -1,7 +1,5 @@
 class RenameCountryAndState < ActiveRecord::Migration
   def change
-    change_column :job_posts, :country, :integer
-    change_column :job_posts, :state, :integer
     rename_column :job_posts, :country, :country_id
     rename_column :job_posts, :state, :state_id
   end
